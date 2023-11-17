6c97d3cd41aeccb666d30dc2ac25e7e88a3a3cce
Nightfeller <family2earls@gmail.com>
2023-11-16 19:23:55 -0600

    changed whitespace for .handlebars and .js files.

--- public/js/charactercreate.js
+++ public/js/charactercreate.js
@@ -1,48 +1,47 @@
 const rng = require('rng')
 
 const characterFormHandler = async (event) => {
-    event.preventDefault();
-  
-    // Collect values from the character form
-    const charName = document.querySelector('#character-name').value.trim();
-    const charRace = document.querySelector('#character-race').value.trim();
-    const charClass = document.querySelector('#character-class').value.trim();
-    const abilityScores = document.querySelector('#ability-scores').value.trim();
-    
+  event.preventDefault();
 
-    if (charName && charRace && charClass && abilityScores) {
-      
-      charAbilityScores = setAbilityScore(abilityScores.value);
-      // Send a POST request to the API endpoint
-      }
-      const response = await fetch('/api/characters/new-character', {
-        method: 'POST',
-        body: JSON.stringify({ charName, charRace, charClass, charAbilityScores}),
-        headers: { 'Content-Type': 'application/json' },
-      });
+    // Collect values from the character form
+  const charName = document.querySelector('#character-name').value.trim();
+  const charRace = document.querySelector('#character-race').value.trim();
+  const charClass = document.querySelector('#character-class').value.trim();
+  const abilityScores = document.querySelector('#ability-scores').value.trim();
   
-      if (response.ok) {
+
+  if (charName && charRace && charClass && abilityScores) {
+    charAbilityScores = setAbilityScore(abilityScores.value);
+      // Send a POST request to the API endpoint
+  }
+  const response = await fetch('/api/characters/new-character', {
+    method: 'POST',
+    body: JSON.stringify({ charName, charRace, charClass, charAbilityScores}),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  
+  if (response.ok) {
         // If successful, redirect the browser to the profile page
-        document.location.replace('/profile');
-      } else {
-        alert(response.statusText);
-      }
+    document.location.replace('/profile');
+  } else {
+    alert(response.statusText);
+  }
 };
 
 function setAbilityScore(abilityScoresChoice, classChoice) {
-    const abilityScores = []
-    switch(choice.value) {
-        case 'random':
-            for (let i = 0; i < 7; i++) {
-                abilityScoresChoice += Math.random() * (20 - 1) + 1;
-            } ;
-        case 'recommended':
+  const abilityScores = []
+  switch(choice.value) {
+    case 'random':
+      for (let i = 0; i < 7; i++) {
+        abilityScoresChoice += Math.random() * (20 - 1) + 1;
+      };
+    case 'recommended':
             // choose ability scores based on selected class 
     return abilityScores
-    }
+  }
 };
   
 document
-    .querySelector('.character-form')
-    .addEventListener('submit', characterFormHandler);
+  .querySelector('.character-form')
+  .addEventListener('submit', characterFormHandler);
     
