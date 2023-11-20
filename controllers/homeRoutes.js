f53eec816b9eb5dc7fe9f172c486148eefaa1cf3
InternetEthan <internetethan@gmail.com>
2023-11-20 01:57:16 -0600

    added pages for character sheets

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -38,7 +38,26 @@ router.get('/profile', withAuth, async (req, res) => {
 
   
   // display a specific character
-  router.get('characters/:id', async (req, res) => {
-  });
+router.get('/characters/:id', async (req, res) => {
+  try {
+    const characterData = await Character.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const character = characterData.get({ plain: true });
+
+    res.render('characterSheet', {
+      ...character,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
